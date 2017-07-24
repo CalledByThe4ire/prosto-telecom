4d25d71b060a6717ac2f9731674fea91d10df887
Andrey Karamnoff <karamnoff@gmail.com>
2017-07-25 00:13:38 +0300

    :sparkles: (app/blocks/page-header.page-header.js, app/blocks/offers) Add js to toggle 'offers' block, edit styles & markup for 'page-header' & 'offers'

---
+++ app/blocks/page-header/page-header.js
@@ -0,0 +1,84 @@
+// import utils from '../../scripts/utils';
+
+export default window.addEventListener(`DOMContentLoaded`, () => {
+  // media queries
+  const mqlMobile = window.matchMedia(`only screen and (max-width: 1071px)`);
+  const mqlDesktop = window.matchMedia(`only screen and (min-width: 1072px)`);
+
+  // DOM elements
+  const containerFluid = document.querySelector(`[data-js='container-fluid']`);
+  const pageHeader = document.querySelector(`[data-js='page-header']`);
+  const logoLink = document.querySelector(`[data-js='logo-link']`);
+  const offers = document.querySelector(`[data-js='offers']`);
+  const toggle = document.querySelector(`[data-js='offers-toggle']`);
+
+  // modifiers
+  const closeMod = `close`;
+  const visibleMod = `visible`;
+  const colorizedMod = `colorized`;
+
+  /**
+   * Transform 'page-header' block appearance via classes
+   * for different viewport' modes
+   * @param  {String} viewPortMode
+   */
+  const pageHeaderDesign = viewPortMode => {
+    switch (viewPortMode) {
+      case `mobile`:
+        containerFluid.classList.add(`page-header__container-fluid--` + colorizedMod);
+        pageHeader.classList.add(`page-header--` + colorizedMod);
+        logoLink.classList.remove(`page-header__link--` + visibleMod);
+        toggle.classList.add(`page-header__toggle--` + closeMod);
+        break;
+      case `desktop`:
+        containerFluid.classList.remove(`page-header__container-fluid--` + colorizedMod);
+        pageHeader.classList.remove(`page-header--` + colorizedMod);
+        logoLink.classList.add(`page-header__link--` + visibleMod);
+        toggle.classList.remove(`page-header__toggle--` + closeMod);
+        break;
+    }
+  };
+
+	/**
+	 * Toggle 'offers' block for mobile
+	 * @param {MouseEvent} evt
+	 */
+  const toggleOffers = evt => {
+    evt.target.classList.toggle(`page-header__toggle--` + closeMod);
+    offers.classList.toggle(`offers--` + visibleMod);
+
+    if (!offers.classList.contains(`offers--` + visibleMod)) {
+      pageHeaderDesign(`desktop`);
+    } else {
+      pageHeaderDesign(`mobile`);
+    }
+  };
+
+  /**
+ * Toggle 'offers' block conditionally for different breakpoints
+ */
+  const matchMedia = () => {
+    if (mqlDesktop.matches) {
+      if (!offers.classList.contains(`offers--` + visibleMod)) {
+        offers.classList.add(`offers--` + visibleMod);
+      }
+    }
+
+    if (mqlMobile.matches) {
+      if (offers.classList.contains(`offers--` + visibleMod)) {
+        pageHeaderDesign(`mobile`);
+      }
+    }
+  };
+
+  toggle.addEventListener(`click`, evt => {
+    toggleOffers(evt);
+  });
+
+  matchMedia();
+
+  window.addEventListener(`resize`, () => {
+    matchMedia();
+  });
+
+});
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,7 @@
 import svg4everybody from 'svg4everybody';
 
+import '../blocks/page-header/page-header.js';
+
 document.addEventListener(`DOMContentLoaded`, () => {
   svg4everybody();
 });
