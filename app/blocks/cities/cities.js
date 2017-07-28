487c186a53960ab64a9ec5348fdb79f06dc9ffd8
Andrey Karamnoff <karamnoff@gmail.com>
2017-07-28 11:46:44 +0300

    :sparkles: :art: (app/blocks/cities, app/blocks/select) Refactor 'cities' block

--- app/blocks/cities/cities.js
+++ app/blocks/cities/cities.js
@@ -3,14 +3,15 @@ import utils from '../../scripts/utils';
 export default window.addEventListener(`DOMContentLoaded`, () => {
   // DOM elements
   const citiesWrapper = document.querySelector(`[data-js='cities-wrapper']`);
-  const citiesSelect = document.querySelector(`[data-js='cities-select']`);
-  const citiesItemCollection = utils.$$(`[data-js='cities-item']`);
+  const citiesSelect = document.querySelector(`[data-js='cities'] select`);
+  const citiesOptionCollection = utils.$$(`[data-js='cities'] option`);
 
+  console.log(citiesOptionCollection);
   /**
  * Get selected option' data and display it in parent container
  */
   const changeEventHandler = () => {
-    let selectedItemValue = citiesItemCollection[citiesSelect.selectedIndex].dataset.value;
+    let selectedItemValue = citiesOptionCollection[citiesSelect.selectedIndex].dataset.value;
     citiesWrapper.dataset.value = selectedItemValue;
   };
 
