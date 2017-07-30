33e6d232745fa15dafba057ff012a9bf7f3c1e09
Andrey Karamnoff <karamnoff@gmail.com>
2017-07-30 19:58:29 +0300

    :sparkles: (app/blocks/slider, app/blocks/slide, app/data/slides-list) Add slider

---
+++ app/blocks/slider/slider.js
@@ -0,0 +1,47 @@
+import { lory } from 'lory.js';
+
+export default window.addEventListener(`DOMContentLoaded`, () => {
+  const slider = document.querySelector(`.js_slider`);
+  const dotCount = slider.querySelectorAll(`.js_slide`).length;
+  const dotContainer = slider.querySelector(`.js_slider-dots`);
+  const dotListItem = document.createElement(`li`);
+
+  const handleDotEvent = e => {
+    if (e.type === `before.lory.init`) {
+      for (let i = 0, len = dotCount; i < len; i++) {
+        const clone = dotListItem.cloneNode();
+        dotContainer.appendChild(clone);
+      }
+      dotContainer.childNodes[0].classList.add(`active`);
+    }
+    if (e.type === `after.lory.init`) {
+      for (let i = 0, len = dotCount; i < len; i++) {
+        dotContainer.childNodes[i].addEventListener(`click`, evt => {
+          dotNavigationSlider.slideTo(Array.prototype.indexOf.call(dotContainer.childNodes, evt.target));
+        });
+      }
+    }
+    if (e.type === `after.lory.slide`) {
+      for (let i = 0, len = dotContainer.childNodes.length; i < len; i++) {
+        dotContainer.childNodes[i].classList.remove(`active`);
+      }
+      dotContainer.childNodes[e.detail.currentSlide - 1].classList.add(`active`);
+    }
+    if (e.type === `on.lory.resize`) {
+      for (let i = 0, len = dotContainer.childNodes.length; i < len; i++) {
+        dotContainer.childNodes[i].classList.remove(`active`);
+      }
+      dotContainer.childNodes[0].classList.add(`active`);
+    }
+  };
+
+  slider.addEventListener(`before.lory.init`, handleDotEvent);
+  slider.addEventListener(`after.lory.init`, handleDotEvent);
+  slider.addEventListener(`after.lory.slide`, handleDotEvent);
+  slider.addEventListener(`on.lory.resize`, handleDotEvent);
+
+  const dotNavigationSlider = lory(slider, {
+    infinite: 1,
+    enableMouseEvents: true
+  });
+});
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,13 @@
 import svg4everybody from 'svg4everybody';
 
+// page-header
 import '../blocks/offers/offers.js';
 import '../blocks/page-header/page-header.js';
 import '../blocks/nav/nav.js';
 import '../blocks/cities/cities.js';
+
+// main-content
+import '../blocks/slider/slider.js';
 
 document.addEventListener(`DOMContentLoaded`, () => {
   svg4everybody();
