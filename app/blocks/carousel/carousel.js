63ba4ed036978f328dd054f2edc62f0a89183a60
Andrey Karamnoff <karamnoff@gmail.com>
2017-08-11 21:27:29 +0300

    :sparkles: Add styles for info

--- app/blocks/carousel/carousel.js
+++ app/blocks/carousel/carousel.js
@@ -4,11 +4,24 @@ import Flickity from 'flickity';
 const carouselsCollection = utils.$$(`.carousel`);
 
 carouselsCollection.forEach(carousel => {
-  /*eslint-disable */
-  new Flickity(carousel, {
-    cellSelector: `.carousel__cell`,
-    wrapAround: true
-  });
-  /*eslint-enable */
+  if (carousel.classList.contains(`carousel--promo`)) {
+    /*eslint-disable */
+    new Flickity(carousel, {
+      cellSelector: `.carousel__cell`,
+      wrapAround: true
+    });
+    /*eslint-enable */
+  }
+
+  if (carousel.classList.contains(`carousel--info`)) {
+    /*eslint-disable */
+    new Flickity(carousel, {
+      cellSelector: `.carousel__cell`,
+      prevNextButtons: false,
+      watchCSS: true,
+      wrapAround: true
+    });
+    /*eslint-enable */
+  }
 });
 
