4096777f6e81967fae85832cafb8b6fda84457a0
Andrey Karamnoff <karamnoff@gmail.com>
2017-08-12 15:01:46 +0300

    :sparkles: (app/blocks/page-header) Add logo's colorized version

--- app/blocks/page-header/page-header.js
+++ app/blocks/page-header/page-header.js
@@ -27,14 +27,14 @@ export default window.addEventListener(`DOMContentLoaded`, () => {
         containerFluid.classList.add(`page-header__container-fluid--` + colorizedMod);
         containerFluid.classList.add(`page-header__container-fluid--` + absPosMod);
         pageHeader.classList.add(`page-header--` + colorizedMod);
-        logoLink.classList.remove(`page-header__link--` + visibleMod);
+        logoLink.classList.add(`page-header__link--` + colorizedMod);
         toggle.classList.add(`page-header__toggle--` + closeMod);
         break;
       case `desktop`:
         containerFluid.classList.remove(`page-header__container-fluid--` + colorizedMod);
         containerFluid.classList.remove(`page-header__container-fluid--` + absPosMod);
         pageHeader.classList.remove(`page-header--` + colorizedMod);
-        logoLink.classList.add(`page-header__link--` + visibleMod);
+        logoLink.classList.remove(`page-header__link--` + colorizedMod);
         toggle.classList.remove(`page-header__toggle--` + closeMod);
         break;
     }
