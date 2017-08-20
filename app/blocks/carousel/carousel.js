469029c80687024b54f9f90270465141c63627b6
Andrey Karamnoff <karamnoff@gmail.com>
2017-08-20 18:56:06 +0300

    :sparkles: (app/carousel) Add similar ajax request for second slider in 'info' group

--- app/blocks/carousel/carousel.js
+++ app/blocks/carousel/carousel.js
@@ -22,11 +22,15 @@ export default window.addEventListener(`DOMContentLoaded`, () => {
   };
 
   // carousel' toggle
-  // const infoToggleCollection = utils.$$(`[data-js='info-toggle']`);
+  const infoToggleCollection = utils.$$(`[data-js='info-toggle']`);
+  const infoToggleChecked = document.querySelector(`[data-js='info-toggle'][checked='checked']`);
 
   // template
   const templateElement = document.querySelector(`[data-js='template']`);
   const slideToClone = templateElement.innerHTML;
+
+  // xhr
+  let xhrList = {};
 
   /**
    * Creates carousel's cell
@@ -77,6 +81,51 @@ export default window.addEventListener(`DOMContentLoaded`, () => {
     return carouselContent;
   };
 
+  /**
+  * Enable / disable carousel for different viewports
+  */
+  const matchMedia = () => {
+    carouselsCollection.forEach(carousel => {
+      if (carousel.parentElement.dataset.modifier === infoToggleChecked.dataset.modifier) {
+        if (mqlDesktop.matches) {
+          flkty.destroy();
+        }
+
+        if (mqlMobile.matches) {
+          // Ajax issues
+          flkty.destroy();
+          flkty = new Flickity(carousel, infoFlickity);
+        }
+      }
+    });
+  };
+
+  const makeXhr = () => {
+    // Ajax Call
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        // On failure
+        if (xhr.status !== 200) {
+          throw new Error(xhr.status + `: ` + xhr.statusText);
+        } else {
+          const dataObject = JSON.parse(xhr.responseText);
+          console.log(dataObject);
+        }
+      }
+    };
+    return xhr;
+  };
+
+  xhrList[`news`] = makeXhr();
+  xhrList[`issues`] = makeXhr();
+
+  Object.keys(xhrList).forEach(xhr => {
+    xhrList[xhr].open(`GET`, `http://localhost:3000/assets/data/${xhr}-slides.json`, true);
+    xhrList[xhr].setRequestHeader(`Access-Control-Allow-Origin`, `*`);
+    xhrList[xhr].send();
+  });
+
   carouselsCollection.forEach(carousel => {
     if (carousel.classList.contains(`carousel--promo`)) {
       // Ajax issues
@@ -88,77 +137,27 @@ export default window.addEventListener(`DOMContentLoaded`, () => {
     }
   });
 
-  /**
-  * Enable / disable carousel for different viewports
-  */
-  const matchMedia = () => {
-    if (mqlDesktop.matches) {
-      flkty.destroy();
-    }
-
-    if (mqlMobile.matches) {
-      flkty = new Flickity(`.carousel--info`, infoFlickity);
-    }
-  };
-
   matchMedia();
 
   window.addEventListener(`resize`, () => {
     matchMedia();
   });
 
-  // Ajax Call
-  const xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      // On failure
-      if (xhr.status !== 200) {
-        throw new Error(xhr.status + `: ` + xhr.statusText);
-        // On success
-      } else {
-        // Convert JSON string response to an Object
-        const dataObject = JSON.parse(xhr.responseText);
-        if (mqlMobile.matches) {
-          flkty.append(getContentForMobile(dataObject));
-        }
-        if (mqlDesktop.matches) {
-          document.querySelector(`.carousel--info`).innerHTML = getContentForDesktop(dataObject);
+  const toggleCarousel = evt => {
+    carouselsCollection.forEach(carousel => {
+
+      if (carousel.classList.contains(`carousel--info`)) {
+        if (evt.target.dataset.modifier === carousel.parentElement.dataset.modifier) {
+          flkty.destroy();
+          flkty = new Flickity(carousel, infoFlickity);
         }
       }
-    }
+
+    });
   };
 
-  xhr.open(`GET`, `http://localhost:3000/assets/data/news-slides.json`, true);
-  xhr.setRequestHeader(`Access-Control-Allow-Origin`, `*`);
-  xhr.send();
-
-  // const toggleCarousel = evt => {
-  //   carouselsCollection.forEach(carousel => {
-  //     let flkty = new Flickity(carousel, {
-  //       cellSelector: `.carousel__cell`,
-  //       prevNextButtons: false,
-  //       wrapAround: true
-  //     });
-
-  //     if (carousel.classList.contains(`carousel--info`)) {
-  //       flkty.destroy();
-
-  //       if (evt.target.dataset.modifier === carousel.parentElement.dataset.modifier) {
-  //         flkty = new Flickity(carousel, {
-  //           cellSelector: `.carousel__cell`,
-  //           prevNextButtons: false,
-  //           watchCSS: true,
-  //           wrapAround: true
-  //         });
-  //       }
-
-  //     }
-
-  //   });
-  // };
-
-  // infoToggleCollection.forEach(element => {
-  //   element.addEventListener(`click`, toggleCarousel);
-  // });
+  infoToggleCollection.forEach(element => {
+    element.addEventListener(`click`, toggleCarousel);
+  });
 });
 
